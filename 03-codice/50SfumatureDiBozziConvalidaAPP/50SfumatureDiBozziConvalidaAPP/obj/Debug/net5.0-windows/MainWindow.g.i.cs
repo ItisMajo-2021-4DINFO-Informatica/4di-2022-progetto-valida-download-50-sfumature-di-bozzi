09142d338830d932db97a91036c32f9059e2bce6
Matteo Decisi <m.decisi04@gmail.com>
2022-03-02 14:07:38 +0100

    Risoluzione dei Problemi
    Controllo e Risoluzione dei problemi riscontrati.

--- 03-codice/50SfumatureDiBozziConvalidaAPP/50SfumatureDiBozziConvalidaAPP/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ 03-codice/50SfumatureDiBozziConvalidaAPP/50SfumatureDiBozziConvalidaAPP/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace _50SfumatureDiBozziConvalidaAPP {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/50SfumatureDiBozziConvalidaAPP;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/50SfumatureDiBozziConvalidaAPP;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
