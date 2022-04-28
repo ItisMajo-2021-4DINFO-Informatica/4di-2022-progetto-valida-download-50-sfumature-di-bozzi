d540edc1865208bceb6a09339100064b86c0f993
Matteo Decisi <m.decisi04@gmail.com>
2022-04-28 22:03:07 +0200

    Ulteriori Modifiche.

--- 03-codice/50SfumatureDiBozziConvalidaAPP/50SfumatureDiBozziConvalidaAPP/MainWindow.xaml.cs
+++ 03-codice/50SfumatureDiBozziConvalidaAPP/50SfumatureDiBozziConvalidaAPP/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.IO;
 using Microsoft.Win32;
 using System.Security.Cryptography;
 using System.Diagnostics;
+using PgpCore;
+using Org.BouncyCastle.Bcpg.OpenPgp;
 
 namespace _50SfumatureDiBozziConvalidaAPP
 {
@@ -24,6 +26,7 @@ namespace _50SfumatureDiBozziConvalidaAPP
     /// </summary>
     public partial class MainWindow : Window
     {
+        static string basePath = @"C:\DemoFiles\";
         string nomeFile;
         string stringainiziale = "";
         string stringafinale = "";
@@ -95,28 +98,103 @@ namespace _50SfumatureDiBozziConvalidaAPP
             }
 
         }
-
-        private void btnVerificaPGP_Click(object sender, RoutedEventArgs e)
+        private void btnVerifica_Click(object sender, RoutedEventArgs e)
         {
-            ProcessStartInfo pInfo = new ProcessStartInfo("cmd.exe");
-            pInfo.WorkingDirectory = @"C:\Windows\System32";
-
-            Process p = Process.Start(pInfo);
         }
+        static async Task<string> Verifica(string publicKeyFilename, string signedFilename)
+        {
+            FileInfo publicKey = new FileInfo(basePath + publicKeyFilename);
+            EncryptionKeys encryptionKeys = new EncryptionKeys(publicKey);
+
+            FileInfo inputFile = new FileInfo(basePath + signedFilename);
+
+            PGP pgp = new PGP(encryptionKeys);
+            bool verified = await pgp.VerifyFileAsync(inputFile);
+
+            if (verified)
+            {
+                return "The content is signed with the given public key";
+            }
+            else
+            {
+                return "WARNING The content is NOT signed with the given public key";
+            }
+        }
+
+        /*public class iTransactVerifier
+        {
+            private const string PublicKey = @"-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: 4.5
+
+mQCNAjZu
+-----END PGP PUBLIC KEY BLOCK-----";
+
+            public static bool Verify(string signature, string data)
+            {
+                var inputStream = ASCIIEncoding.ASCII.GetBytes(signature);
+
+                PgpPublicKey publicKey = ReadPublicKeyFromString();
+                var stream = PgpUtilities.GetDecoderStream(inputStream);
+
+                PgpObjectFactory pgpFact = new PgpObjectFactory(stream);
+
+                PgpSignatureList sList = pgpFact.NextPgpObject() as PgpSignatureList;
+                if (sList == null)
+                {
+                    throw new InvalidOperationException("PgpObjectFactory could not create signature list");
+                }
+                PgpSignature firstSig = sList[0];
+
+                firstSig.InitVerify(publicKey);
+                firstSig.Update(Encoding.UTF8.GetBytes(data));
+
+                var verified = firstSig.Verify();
+                return verified;
+            }
+
+    private static PgpPublicKey ReadPublicKeyFromString()
+            {
+                var varstream = ASCIIEncoding.ASCII.GetBytes(PublicKey);
+                var stream = PgpUtilities.GetDecoderStream(varstream);
+
+                PgpObjectFactory pgpFact = new PgpObjectFactory(stream);
+                var keyRing = (PgpPublicKeyRing)pgpFact.NextPgpObject();
+                return keyRing.GetPublicKey();
+            }
+        }*
+
+        /*ProcessStartInfo pInfo = new ProcessStartInfo("cmd.exe");
+        pInfo.WorkingDirectory = @"C:\Windows\System32";
+
+        Process p = Process.Start(pInfo);*/
 
         private void btnNascondi_Click(object sender, RoutedEventArgs e)
         {
-
+            lblx1.Visibility = Visibility.Hidden;
+            lblx2.Visibility = Visibility.Hidden;
+            lblx4.Visibility = Visibility.Hidden;
+            lblx5.Visibility = Visibility.Hidden;
+            btnCopia.Visibility = Visibility.Hidden;
+            btnCopia2.Visibility = Visibility.Hidden;
+            btnCopia3.Visibility = Visibility.Hidden;
+            btnLink.Visibility = Visibility.Hidden;
         }
 
         private void btnMostra_Click(object sender, RoutedEventArgs e)
         {
-
+            lblx1.Visibility = Visibility.Visible;
+            lblx2.Visibility = Visibility.Visible;
+            lblx4.Visibility = Visibility.Visible;
+            lblx5.Visibility = Visibility.Visible;
+            btnCopia.Visibility = Visibility.Visible;
+            btnCopia2.Visibility = Visibility.Visible;
+            btnCopia3.Visibility = Visibility.Visible;
+            btnLink.Visibility = Visibility.Visible;
         }
 
         private void btnCopia_Click(object sender, RoutedEventArgs e)
         {
-            string xyz = "setx /M path \"%PATH%,C:\\Program Files (x86)\\Gpg4win\\..\\GnuPG\\bin\"";
+            string xyz = "SET PATH=C:\\Program Files (x86)\\Gpg4win\\..\\GnuPG\\bin";
 
             if (xyz != "")
             {
@@ -132,6 +210,22 @@ namespace _50SfumatureDiBozziConvalidaAPP
             if (abc != "")
             {
                 Clipboard.SetText(abc);
+                MessageBox.Show("linea di comando copiata negli appunti.");
+            }
+        }
+
+        private void btnLink_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCopia3_Click(object sender, RoutedEventArgs e)
+        {
+            string poi = "gpg --verify C:\\openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256.asc C:\\openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256";
+
+            if (poi != "")
+            {
+                Clipboard.SetText(poi);
                 MessageBox.Show("linea di comando copiata negli appunti.");
             }
         }
