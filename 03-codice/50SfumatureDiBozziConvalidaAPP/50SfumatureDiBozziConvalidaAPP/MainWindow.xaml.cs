7cd7ac91f60308baf9e5f6fbb846e29046afd383
Matteo Decisi <m.decisi04@gmail.com>
2022-05-04 19:44:50 +0200

    Modifiche.

--- 03-codice/50SfumatureDiBozziConvalidaAPP/50SfumatureDiBozziConvalidaAPP/MainWindow.xaml.cs
+++ 03-codice/50SfumatureDiBozziConvalidaAPP/50SfumatureDiBozziConvalidaAPP/MainWindow.xaml.cs
@@ -103,90 +103,127 @@ namespace _50SfumatureDiBozziConvalidaAPP
             string asc = "C:\\Users\\Utente\\Desktop\\openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256.asc";
             string sha = "C:\\Users\\Utente\\Desktop\\openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256";
 
-            using (Process pProcess = new Process())
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "cmd.exe";
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            start.Arguments = "/k gpg --recv-keys 3DBDC284";
+            start.Arguments = "/k gpg --verify " + asc + " " + sha;
+
+            using (Process pProcess = Process.Start(start))
             {
-                pProcess.StartInfo.FileName = "cmd.exe";
-                pProcess.StartInfo.Arguments = "/k gpg --recv-keys 3DBDC284";
-                pProcess.StartInfo.Arguments = "/k gpg --verify " + asc + " " + sha;
-                pProcess.StartInfo.UseShellExecute = false;
-                pProcess.StartInfo.RedirectStandardOutput = true;
-                pProcess.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+                using FileStream flusso = new FileStream("ciaociao.txt", FileMode.OpenOrCreate, FileAccess.Write)
                 {
-                    Frame.Dispatcher.Invoke(() => { Frame.Content += e.Data + Environment.NewLine; });
-                });
+                    StreamReader reader = new StreamReader(flusso);
 
-                pProcess.Start();
+                    for (; ; )
+                    {
+                    string textLine = reader.ReadLine();
 
-            }
+                    if (textLine == null)
+                        break;
+
+                    writer.WriteLine(textLine);
+                    }
             
-        }
-      
-
-        /*ProcessStartInfo pInfo = new ProcessStartInfo("cmd.exe");
-        pInfo.WorkingDirectory = @"C:\Windows\System32";
-
-        Process p = Process.Start(pInfo);*/
-
-        private void btnNascondi_Click(object sender, RoutedEventArgs e)
-        {
-            lblx1.Visibility = Visibility.Hidden;
-            lblx2.Visibility = Visibility.Hidden;
-            lblx4.Visibility = Visibility.Hidden;
-            lblx5.Visibility = Visibility.Hidden;
-            btnCopia.Visibility = Visibility.Hidden;
-            btnCopia2.Visibility = Visibility.Hidden;
-            btnCopia3.Visibility = Visibility.Hidden;
-            btnLink.Visibility = Visibility.Hidden;
-        }
-
-        private void btnMostra_Click(object sender, RoutedEventArgs e)
-        {
-            lblx1.Visibility = Visibility.Visible;
-            lblx2.Visibility = Visibility.Visible;
-            lblx4.Visibility = Visibility.Visible;
-            lblx5.Visibility = Visibility.Visible;
-            btnCopia.Visibility = Visibility.Visible;
-            btnCopia2.Visibility = Visibility.Visible;
-            btnCopia3.Visibility = Visibility.Visible;
-            btnLink.Visibility = Visibility.Visible;
-        }
-
-        private void btnCopia_Click(object sender, RoutedEventArgs e)
-        {
-            string xyz = "SET PATH=C:\\Program Files (x86)\\Gpg4win\\..\\GnuPG\\bin";
-
-            if (xyz != "")
-            {
-                Clipboard.SetText(xyz);
-                MessageBox.Show("linea di comando copiata negli appunti.");
             }
-        }
 
-        private void btnCopia2_Click(object sender, RoutedEventArgs e)
-        {
-            string abc = "gpg --recv-keys 3DBDC284";
-
-            if (abc != "")
+            if (File.Exists("ciaociao.txt"))
             {
-                Clipboard.SetText(abc);
-                MessageBox.Show("linea di comando copiata negli appunti.");
+                FileInfo info = new FileInfo("ciaociao.txt");
+
+                // if the error info is empty or just contains eof etc.
+
+                if (info.Length < 4)
+                    info.Delete();
             }
+
         }
 
-        private void btnLink_Click(object sender, RoutedEventArgs e)
+
+
+
+    /*(using (StreamReader lettore = pProcess.StandardOutput)
+    {
+        string result = lettore.ReadToEnd();
+        Frame.Content = result;
+    }
+    pProcess.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+    {
+        Frame.Dispatcher.Invoke(() => { Frame.Content += e.Data + Environment.NewLine; });
+    });
+
+    pProcess.Start();*/
+
+
+
+
+
+    /*ProcessStartInfo pInfo = new ProcessStartInfo("cmd.exe");
+    pInfo.WorkingDirectory = @"C:\Windows\System32";
+
+    Process p = Process.Start(pInfo);*/
+
+    private void btnNascondi_Click(object sender, RoutedEventArgs e)
+    {
+        lblx1.Visibility = Visibility.Hidden;
+        lblx2.Visibility = Visibility.Hidden;
+        lblx4.Visibility = Visibility.Hidden;
+        lblx5.Visibility = Visibility.Hidden;
+        btnCopia.Visibility = Visibility.Hidden;
+        btnCopia2.Visibility = Visibility.Hidden;
+        btnCopia3.Visibility = Visibility.Hidden;
+        btnLink.Visibility = Visibility.Hidden;
+    }
+
+    private void btnMostra_Click(object sender, RoutedEventArgs e)
+    {
+        lblx1.Visibility = Visibility.Visible;
+        lblx2.Visibility = Visibility.Visible;
+        lblx4.Visibility = Visibility.Visible;
+        lblx5.Visibility = Visibility.Visible;
+        btnCopia.Visibility = Visibility.Visible;
+        btnCopia2.Visibility = Visibility.Visible;
+        btnCopia3.Visibility = Visibility.Visible;
+        btnLink.Visibility = Visibility.Visible;
+    }
+
+    private void btnCopia_Click(object sender, RoutedEventArgs e)
+    {
+        string xyz = "SET PATH=C:\\Program Files (x86)\\Gpg4win\\..\\GnuPG\\bin";
+
+        if (xyz != "")
         {
-
-        }
-
-        private void btnCopia3_Click(object sender, RoutedEventArgs e)
-        {
-            string poi = "gpg --verify C:\\openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256.asc C:\\openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256";
-
-            if (poi != "")
-            {
-                Clipboard.SetText(poi);
-                MessageBox.Show("linea di comando copiata negli appunti.");
-            }
+            Clipboard.SetText(xyz);
+            MessageBox.Show("linea di comando copiata negli appunti.");
         }
     }
+
+    private void btnCopia2_Click(object sender, RoutedEventArgs e)
+    {
+        string abc = "gpg --recv-keys 3DBDC284";
+
+        if (abc != "")
+        {
+            Clipboard.SetText(abc);
+            MessageBox.Show("linea di comando copiata negli appunti.");
+        }
+    }
+
+    private void btnLink_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnCopia3_Click(object sender, RoutedEventArgs e)
+    {
+        string poi = "gpg --verify C:\\openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256.asc C:\\openSUSE-Leap-15.3-3-NET-x86_64-Build38.1-Media.iso.sha256";
+
+        if (poi != "")
+        {
+            Clipboard.SetText(poi);
+            MessageBox.Show("linea di comando copiata negli appunti.");
+        }
+    }
+}
 }
